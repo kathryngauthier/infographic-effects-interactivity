e060fb9a5afd63a5b5b0042ed28bf339ca581665
kathryngauthier <Kathryn@Kathryns-MacBook-Pro.local>
2016-04-29 01:54:32 -0400

    centered

--- js/main.js
+++ js/main.js
@@ -8,29 +8,29 @@ var $star6 = $('.star6');
 $star1.waypoint(function () {
   $star1.addClass('js-star1-animate');
   console.log('Waypoint!');
-}, { offset: '50%'});
+}, {offset: '50%'});
 
 $star2.waypoint(function () {
   $star2.addClass('js-star2-animate');
   console.log('Waypoint!');
-}, { offset: '50%'});
+}, {offset: '50%'});
 
 $star3.waypoint(function () {
   $star3.addClass('js-star3-animate');
   console.log('Waypoint!');
-}, { offset: '50%'});
+}, {offset: '50%'});
 
 $star3.waypoint(function () {
   $star4.addClass('js-star4-animate');
   console.log('Waypoint!');
-}, { offset: '50%'});
+}, {offset: '50%'});
 
 $star5.waypoint(function () {
   $star5.addClass('js-star5-animate');
   console.log('Waypoint!');
-}, { offset: '50%'});
+}, {offset: '50%'});
 
 $star6.waypoint(function () {
   $star6.addClass('js-star6-animate');
   console.log('Waypoint!');
-}, { offset: '50%'});
+}, {offset: '50%'});
