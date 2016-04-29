acb85a5340ccd30043fca814f86412fb6b5f08af
kathryngauthier <Kathryn@Kathryns-MacBook-Pro.local>
2016-04-29 01:16:45 -0400

    js

--- js/main.js
+++ js/main.js
@@ -1,41 +1,36 @@
 var $star1 = $('.star1');
-
-$star1.waypoint(function () {
-	$star1.addClass('js-star1-animate');
-	console.log('Waypoint!');
-},{ offset: '50%'});
-
 var $star2 = $('.star2');
-
-$star2.waypoint(function () {
-	$star2.addClass('js-star2-animate');
-	console.log('Waypoint!');
-},{ offset: '50%'});
-
 var $star3 = $('.star3');
-
-$star3.waypoint(function () {
-	$star3.addClass('js-star3-animate');
-	console.log('Waypoint!');
-},{ offset: '50%'});
-
 var $star4 = $('.star4');
-
-$star3.waypoint(function () {
-	$star4.addClass('js-star4-animate');
-	console.log('Waypoint!');
-},{ offset: '50%'});
-
 var $star5 = $('.star5');
-
-$star5.waypoint(function () {
-	$star5.addClass('js-star5-animate');
-	console.log('Waypoint!');
-},{ offset: '50%'});
-
 var $star6 = $('.star6');
 
+$star1.waypoint(function () {
+  $star1.addClass('js-star1-animate');
+  console.log('Waypoint!');
+}, { offset: '50%'});
+
+$star2.waypoint(function () {
+  $star2.addClass('js-star2-animate');
+  console.log('Waypoint!');
+}, { offset: '50%'});
+
+$star3.waypoint(function () {
+  $star3.addClass('js-star3-animate');
+  console.log('Waypoint!');
+}, { offset: '50%'});
+
+$star3.waypoint(function () {
+  $star4.addClass('js-star4-animate');
+  console.log('Waypoint!');
+}, { offset: '50%'});
+
+$star5.waypoint(function () {
+  $star5.addClass('js-star5-animate');
+  console.log('Waypoint!');
+}, { offset: '50%'});
+
 $star6.waypoint(function () {
-	$star6.addClass('js-star6-animate');
-	console.log('Waypoint!');
-},{ offset: '50%'});
+  $star6.addClass('js-star6-animate');
+  console.log('Waypoint!');
+}, { offset: '50%'});
