1c26d847e304bdb773d4bf752105a0b19c06e238
kathryngauthier <Kathryn@Kathryns-MacBook-Pro.local>
2016-04-29 23:42:44 -0400

    removed bottom

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@ var $star3 = $('.star3');
 var $star4 = $('.star4');
 var $star5 = $('.star5');
 var $star6 = $('.star6');
+var $thing1 = $('.thing1');
+var $thing2 = $('.thing2');
+var $thing3 = $('.thing3');
+var $thing4 = $('.thing4');
 
 $star1.waypoint(function () {
   $star1.addClass('js-star1-animate');
@@ -32,5 +36,25 @@ $star5.waypoint(function () {
 
 $star6.waypoint(function () {
   $star6.addClass('js-star6-animate');
+  console.log('Waypoint!');
+}, {offset: '50%'});
+
+$thing1.waypoint(function () {
+  $thing1.addClass('js-thing1-animate');
+  console.log('Waypoint!');
+}, {offset: '50%'});
+
+$thing2.waypoint(function () {
+  $thing2.addClass('js-thing2-animate');
+  console.log('Waypoint!');
+}, {offset: '50%'});
+
+$thing3.waypoint(function () {
+  $thing3.addClass('js-thing3-animate');
+  console.log('Waypoint!');
+}, {offset: '50%'});
+
+$thing4.waypoint(function () {
+  $thing4.addClass('js-thing4-animate');
   console.log('Waypoint!');
 }, {offset: '50%'});
